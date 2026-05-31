34b9a2c488a350dfc9b24d3fcc4a246fa6573f36
Faizan Ahmed <faizanahmed1295@gmail.com>
2026-05-31 21:30:28 +0500

    Updattion

---
+++ src/app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://kerodevs.com", lastModified: new Date() },
+    { url: "https://kerodevs.com/about", lastModified: new Date() },
+    { url: "https://kerodevs.com/services", lastModified: new Date() },
+    { url: "https://kerodevs.com/contact", lastModified: new Date() },
+  ]
+}
