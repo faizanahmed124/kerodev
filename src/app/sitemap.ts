2ec7ea20ae20e824d823d50df65a3d61959b7c32
Faizan Ahmed <faizanahmed1295@gmail.com>
2026-05-31 21:33:18 +0500

    site map updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,8 @@
-export default function sitemap() {
+import { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: "https://kerodevs.com", lastModified: new Date() },
     { url: "https://kerodevs.com/about", lastModified: new Date() },
