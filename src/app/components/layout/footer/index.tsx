4a55a31de9265097616324b658a3d48d21b49b7c
Faizan Ahmed <faizanahmed1295@gmail.com>
2026-05-31 20:43:41 +0500

    Package Updated

--- src/app/components/layout/footer/index.tsx
+++ src/app/components/layout/footer/index.tsx
@@ -173,7 +173,7 @@ const Footer = () => {
                   <span className="kf-contact-icon">
                     <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="#38bdf8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>
                   </span>
-                  Elgin St. Celina, Delaware 10299
+                  275 Barki Rd,Grove Block Paragon City, Lahore, 54000, Pakistan
                 </div>
               </div>
 
