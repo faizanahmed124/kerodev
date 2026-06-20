7d5728dddbdc1b83eee37dc5bc5c764bb05a426c
Faizan Ahmed <faizanahmed1295@gmail.com>
2026-06-21 00:21:51 +0500

    emailint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true
+//   }
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
