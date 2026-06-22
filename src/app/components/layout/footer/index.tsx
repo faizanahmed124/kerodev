b1ffcae33b72b7610dbed2075069be2877197fe2
Faizan Ahmed <faizanahmed1295@gmail.com>
2026-06-22 20:15:39 +0500

    number changed

--- src/app/components/layout/footer/index.tsx
+++ src/app/components/layout/footer/index.tsx
@@ -160,7 +160,7 @@ const Footer = () => {
                   <span className="kf-contact-icon">
                     <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="#38bdf8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.9 13.5a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.8 2.72h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 10.1a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
                   </span>
-                  (+92) 315-744-974
+                  (+92) 304 6925046
                 </Link>
 
                 <Link href="mailto:hi@kerodevs.com" className="kf-contact-item">
